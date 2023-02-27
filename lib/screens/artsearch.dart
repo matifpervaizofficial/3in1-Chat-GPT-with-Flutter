e3237cbc222f5f1174398f31a343c30e9131a4f6
matifpervaizofficial <matifpervaizofficial@gmail.com>
2023-02-27 23:23:20 +0500

    root directoris and packages

---
+++ lib/screens/artsearch.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ImageSearch extends StatefulWidget {
+  const ImageSearch({super.key});
+
+  @override
+  State<ImageSearch> createState() => _ImageSearchState();
+}
+
+class _ImageSearchState extends State<ImageSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Expanded(child: Container())],
+      ),
+    );
+  }
+}
